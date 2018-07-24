7d8f6310480dcfb65851dd810232aba1a2ed29d0
Ken Crimmins <kencrimmins@Kens-MacBook-Pro.local>
2018-07-23 20:38:56 -0700

    Ready for pull req

--- client/src/components/App.test.jsx
+++ client/src/components/App.test.jsx
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 
 describe('App component', () => {
 
-	it( 'Should have an svg element', () => {
+	it( 'Should create an svg graph with number of points equal to the data passed in', () => {
 		const wrapper = shallow(<App />)
 		const graphData = wrapper.state().graphData
 		expect(graphData.length).toEqual(30)
