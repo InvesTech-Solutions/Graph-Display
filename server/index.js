3d4dfba81c3326ee3c38661f83654ea41a0a9d51
Ken Crimmins <kencrimmins@Kens-MacBook-Pro.local>
2018-07-25 21:12:16 -0700

    begin work on DB implementation

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var bodyParser = require('body-parser')
+var db = require('../DB/')
+
 
 app.use((req, res, next) => {
   console.log(`serving request ${req.method} at ${req.url}`);
   next();
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
@@ -13,3 +19,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> {
   console.log(`Listening on port ${PORT}`);
 });
+
+app.get('/')
