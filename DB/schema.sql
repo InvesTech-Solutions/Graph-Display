a3dd4c27edfc07b6d135d80ac0f7dc584bbf0506
Adam Carter <carter.c.adam@gmail.com>
2018-08-09 14:13:44 -0700

    added readme for crud operations

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,21 +1,23 @@
 DROP DATABASE IF EXISTS company_data;
 CREATE DATABASE company_data;
 
-USE company_data;
+\c company_data;
+
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS price_records_monthly;
 
 CREATE TABLE companies (
-  id int(9) NOT NULL AUTO_INCREMENT,
-  name varchar(100) NOT NULL,
-  last_closing_price decimal(10,2) NOT NULL,
+  id serial,
+  name varchar,
+  last_closing_price money,
   PRIMARY KEY (id)
 ); 
 
 CREATE TABLE `price_records_monthly` (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  companyID int(9) NOT NULL,
-  price decimal(10,2) NOT NULL,
-  price_date datetime(6) NOT NULL,
+  id serial,
+  companyID integer,
+  price money,
+  price_date date,
   PRIMARY KEY (id),
-  FOREIGN KEY (companyID) REFERENCES companies(id)
 );
 
